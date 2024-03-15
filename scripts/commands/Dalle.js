e76b61c485369c127bd6d6de5098b71043d3285a
Sheikh404Cyber <150942663+Sheikh404Cyber@users.noreply.github.com>
2024-03-15 18:35:58 +0600

    Create Dalle.js

---
+++ scripts/commands/Dalle.js
@@ -0,0 +1,48 @@
+const axios = require('axios');
+const fs = require('fs-extra');
+const path = require('path');
+
+module.exports.config = {
+    name: "dalle",
+    version: "1.0.0",
+    permission: 0,
+    credits: "Amin Sheikh",
+    prefix: true,
+    description: "Generate images by Dalle-3 AI",
+    category: "user",
+    usages: "",
+    cooldowns: 0
+};
+
+
+module.exports.run = async function ({ api, event, args }) {
+  const prompt = event.messageReply?.body.split("dalle")[1] ||  args.join(" ");
+  if (!prompt) {
+   return api.sendMessage("❌| Wrong Formet .✅ | Use 17/18 years old boy/girl watching football match on tv and written Dipto and 69 on the back of his Dress , 4k",event.threadID,event.messageID);
+  }
+    try {
+      const w = await api.sendMessage("Wait koro baby < 😽", event.threadID);
+  
+const response = await axios.get(`https://all-image-genator-d1p.onrender.com/dipto/dalle?prompt=${prompt}&key=dipto008&cookies=1WIC5qD_DRlZJlrpj-aF-pw4aA6hfWTA__05tcS3r_b_NBD7ipSHz8Ao7ggyIl3R9MTsdBH9Od8cY1BqSXT0oR8uU-hJ3IbYbPmu3M4eaH6nufOyjkiNE1e0Xy5fnR-XLmISaRdMmaOGJt2eLzGFDNjHwChuH9bxmcuv4G-aTVwpcQvK4tOAe1l8mTe4qI5kYjQ3YtXlJfNVPmmMf8toJ78YgU7Xd3bp9EgMjMWTrL4Y)
+      const data = response.data.imgUrls;
+      if (!data || data.length === 0) {
+        api.sendMessage("Empty response or no images generated.",event.threadID,event.messageID);
+      }
+      const diptoo = [];
+      for (let i = 0; i < data.length; i++) {
+        const imgUrl = data[i];
+        const imgResponse = await axios.get(imgUrl, { responseType: 'arraybuffer' });
+        const imgPath = path.join(__dirname, 'dalle', `${i + 1}.jpg`);
+        await fs.outputFile(imgPath, imgResponse.data);
+        diptoo.push(fs.createReadStream(imgPath));
+      }
+      await api.unsendMessage(w.messageID);
+      await api.sendMessage({
+  body: `✅ |Naw Baby Tumar Generated Pic<😘`,
+        attachment: diptoo
+      },event.threadID, event.messageID);
+    } catch (error) {
+      console.error(error);
+      await api.sendMessage(`Generation failed!\nError: ${error.message}`,event.threadID, event.messageID);
+    }
+  };
