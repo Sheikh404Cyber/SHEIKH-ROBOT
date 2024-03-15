36faafdbc77c19e97dcffc9dae0b3cca504e03f6
Sheikh404Cyber <150942663+Sheikh404Cyber@users.noreply.github.com>
2024-03-15 19:00:46 +0600

    Update Dalle.js

--- scripts/commands/Dalle.js
+++ scripts/commands/Dalle.js
@@ -3,17 +3,17 @@ const fs = require('fs-extra');
 const path = require('path');
 
 module.exports.config = {
-    name: "bing",
-    version: "1.0.0",
-    permission: 0,
-    credits: "Amin Sheikh",
-    prefix: true,
+    name: "dalle",
+    version: "1.0",
+    credits: "dipto",
+    hasPermssion: 0,
+    usePrefix: true,
+   premium: true,
     description: "Generate images by Dalle-3 AI",
-    category: "user",
-    usages: "",
-    cooldowns: 0
-};
-
+    commandCategory: "user",
+    usages: "[text] \nJamon [A 17/18/19 years old boy/girl watching football match on tv and written Dipto and 69 on the back of his Dress , 4k]",
+    cooldowns: 5
+  };
 
 module.exports.run = async function ({ api, event, args }) {
   const prompt = event.messageReply?.body.split("dalle")[1] ||  args.join(" ");
@@ -23,7 +23,7 @@ module.exports.run = async function ({ api, event, args }) {
     try {
       const w = await api.sendMessage("Wait koro baby < 😽", event.threadID);
   
-const response = await axios.get(`https://all-image-genator-d1p.onrender.com/dipto/dalle?prompt=${prompt}&key=dipto008&cookies=1WIC5qD_DRlZJlrpj-aF-pw4aA6hfWTA__05tcS3r_b_NBD7ipSHz8Ao7ggyIl3R9MTsdBH9Od8cY1BqSXT0oR8uU-hJ3IbYbPmu3M4eaH6nufOyjkiNE1e0Xy5fnR-XLmISaRdMmaOGJt2eLzGFDNjHwChuH9bxmcuv4G-aTVwpcQvK4tOAe1l8mTe4qI5kYjQ3YtXlJfNVPmmMf8toJ78YgU7Xd3bp9EgMjMWTrL4Y)
+const response = await axios.get(`https://all-image-genator-d1p.onrender.com/dipto/dalle?prompt=${prompt}&key=dipto008&cookies=1WIC5qD_DRlZJlrpj-aF-pw4aA6hfWTA__05tcS3r_b_NBD7ipSHz8Ao7ggyIl3R9MTsdBH9Od8cY1BqSXT0oR8uU-hJ3IbYbPmu3M4eaH6nufOyjkiNE1e0Xy5fnR-XLmISaRdMmaOGJt2eLzGFDNjHwChuH9bxmcuv4G-aTVwpcQvK4tOAe1l8mTe4qI5kYjQ3YtXlJfNVPmmMf8toJ78YgU7Xd3bp9EgMjMWTrL4Y`)
       const data = response.data.imgUrls;
       if (!data || data.length === 0) {
         api.sendMessage("Empty response or no images generated.",event.threadID,event.messageID);
